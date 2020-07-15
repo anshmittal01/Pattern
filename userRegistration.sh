6d5fe7af1ac50855b95ac0d6747abe8393a4d8d9
Ansh Mittal <mittalansh22@gmail.com>
2020-07-15 14:40:09 +0530

    email validation added

--- userRegistration.sh
+++ userRegistration.sh
@@ -12,6 +12,16 @@ function nameValidator()
 
 }
 
+emailValidator () {
+	email_patt="^[a-z0-9]+([.|_|+|-]?[a-z0-9]+)+?@[a-z0-9]+(\.[a-z]{2,}){1,2}$"
+	if [[ $1 =~ $email_patt ]]
+	then
+		echo 1
+	else
+		echo 0
+	fi
+}
+
 read -p "Enter first name (with Capital first letter) " firstName
 isCorrect=`nameValidator $firstName`
 if [ $isCorrect -eq 1 ]
@@ -27,4 +37,12 @@ then
 	echo "Valid Last Name"
 else
 	echo "Invalid Last Name"
+fi
+read -p "Enter Email ID " email
+isCorrect=`emailValidator $email`
+if [ $isCorrect -eq 1 ]
+then
+	echo "Valid Email"
+else
+	echo "Invalid Email"
 fi
