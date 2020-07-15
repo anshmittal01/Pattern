dacda15779a7b62017f47ee593e01899dfce1003
Ansh Mittal <mittalansh22@gmail.com>
2020-07-15 13:50:30 +0530

    last name validation added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,10 +1,30 @@
 #! /bin/bash
+function nameValidator()
+{
+	firstname_patt="^[A-Z][a-z]{2,}$"
+	if [[ $1 =~ $firstname_patt ]]
+	then
+		echo 1
+	else
+		echo 0
+	fi
 
-read -p "Enter first name (with Capital first letter) " name
-firstname_patt="^[A-Z][a-z]{2,}$"
-if [[ $name =~ $firstname_patt ]]
+
+}
+
+read -p "Enter first name (with Capital first letter) " firstName
+isCorrect=`nameValidator $firstName`
+if [ $isCorrect -eq 1 ]
 then
 	echo "Valid First Name"
 else
 	echo "Invalid First Name"
+fi
+read -p "Enter first name (with Capital first letter) " lastName
+isCorrect=`nameValidator $lastName`
+if [ $isCorrect -eq 1 ]
+then
+	echo "Valid Last Name"
+else
+	echo "Invalid Last Name"
 fi
